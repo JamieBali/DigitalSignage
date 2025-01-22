1391a7a624b6f7098bff1c00c910360fc60833e0
James (Jamie) Bali <92593423+JamieBali@users.noreply.github.com>
2025-01-22 00:50:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,47 +4,56 @@ import os
 import time
 from PIL import Image, ImageTk
 
-def flash_image():
-    root.deiconify()
-    # Randomly select an image path
-    image_path = random.choice(os.listdir())
+first = True
 
-    print(image_path)
+def get_image():
+    # Randomly select an image path
+    image_path = random.choice(os.listdir("\\\\[Storage Machine IP]\\Shared\\DigitalSignage\\"))
 
     # Load the image using PIL
-    image = Image.open(image_path)
+    # We have to append the storage machine path again as the random.choice only returns the file name.
+    image = Image.open("\\\\[Storage Machine IP]\\Shared\\DigitalSignage\\" + image_path)
     iw = image.width
     ih = image.height
     h = root.winfo_screenheight()
-    ratio = iw / ih
+    ratio = iw / ih                 # Calculate ratio to ensure image doesn't get stretched
     w = int(ratio * h)
-    image = image.resize((w, h))  # Resize the image if needed
+    image = image.resize((w, h))    # Resize the image if needed
     photo = ImageTk.PhotoImage(image)
 
+    return photo
+
+def display_image(photo):
+
+    if (photo == False):
+        photo = get_image()
+
+    root.deiconify()
+    
     # Create a label to display the image
     image_label = tk.Label(root, image=photo)
-    image_label.pack()
+    image_label.pack()        
 
-    # Display the image for 2 seconds
+    # Display the image for a given number of seconds (currently 2 mins)
     root.update()
-    time.sleep(2)
+    time.sleep(2) # this is a time in seconds
+    
+    photo = get_image()
 
     # Remove the image label
     image_label.destroy()
 
-    root.withdraw()
-
-    # Schedule the next flash after 10 seconds
-    root.after(1, flash_image)
+    # Schedule the display_image immediately, as the last image just got destroyed.
+    root.after(0, display_image(photo))
 
 # Create the main window
 root = tk.Tk()
-root.title("Image Flasher")
+root.title("Digital Signage")
 root.geometry("400x300")
 root.attributes('-fullscreen', True)
 
-# Start the flashing process
-flash_image()
+# Start signage
+display_image(False)
 
 # Keep the window open
 root.mainloop()
